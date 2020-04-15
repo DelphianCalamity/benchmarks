c015dc8915fc3fdede3c8478b2dc3fe5cd1dd674
Calliope Kostopoulou <kelkost@yahoo.gr>
2020-04-15 09:53:43 +0300

    imagenet logging

--- scripts/tf_cnn_benchmarks/benchmark_cnn.py
+++ scripts/tf_cnn_benchmarks/benchmark_cnn.py
@@ -3733,7 +3733,7 @@ def setup(params):
     hvd.init()
     # wandb.init(project="gradients-compressions-bloom-filter", sync_tensorboard=False)
     # wandb.init(project="gradients-bitstream-compression", sync_tensorboard=False)
-    wandb.init(project="bloomfilter", sync_tensorboard=False)
+    wandb.init(project="imagenet", sync_tensorboard=False)
   platforms_util.initialize(params, create_config_proto(params))
 
   if not params.job_name:
